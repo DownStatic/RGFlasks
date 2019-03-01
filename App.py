2ebcd6fd1bb1142fed6513c0a44176f56ed1d856
Chris Stull <chris.stull@gmail.com>
2019-03-01 16:37:14 -0500

    Change return structure from string to JSON

--- App.py
+++ App.py
@@ -1,11 +1,28 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+flasks = [
+    {
+        'id': 1,
+        'title': u'Eternal Life',
+        'description': u'Prevents death by natural causes and all disease',
+        'origin': u'Western Philosophy',
+        'consumed': False
+    },
+    {
+        'id': 2,
+        'title': u'Arcane Might',
+        'description': u'Grants overflowing arcane magic for a full day.',
+        'origin': u'WoW',
+        'consumed': False
+    }
+]
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
 
 if __name__ == '__main__':
     app.run(debug=True)
